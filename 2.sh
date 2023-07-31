557489fdc07b40ae52fc00c8753d48c97a476d0b
simtelboy <95013297+simtelboy@users.noreply.github.com>
2023-08-01 05:03:39 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -303,19 +303,19 @@ cat > /etc/caddy/Caddyfile << EOF
 # this machine's public IP, then replace ":80" below with your
 # domain name.
 
-:80 {
+#:80 {
 	# Set this path to your site's directory.
-	root * /usr/share/caddy
+	#root * /usr/share/caddy
 
 	# Enable the static file server.
-	file_server
+	#file_server
 
 	# Another common task is to set up a reverse proxy:
 	# reverse_proxy localhost:8080
 
 	# Or serve a PHP site through php-fpm:
 	# php_fastcgi localhost:9000
-}
+#}
 
 # Refer to the Caddy docs for more information:
 # https://caddyserver.com/docs/caddyfile
