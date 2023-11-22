909bd9532003dd18d96877b7cd2b7dbd46e569ba
simtelboy <95013297+simtelboy@users.noreply.github.com>
2023-11-22 10:03:50 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -319,20 +319,20 @@ if [ ! -d /etc/caddy ]; then
 fi
 
 if [ ! -f /etc/caddy/Caddyfile ]; then
-  touch /etc/caddy/Caddyfile
-  chmod +x /etc/caddy/Caddyfile
-cat > /etc/caddy/Caddyfile << EOF	 	
- # The Caddyfile is an easy way to configure your Caddy web server.
- #
- # Unless the file starts with a global options block, the first
- # uncommented line is always the address of your site.
- #
- # To use your own domain name (with automatic HTTPS), first make
- # sure your domain's A/AAAA DNS records are properly pointed to
- # this machine's public IP, then replace ":80" below with your
- # domain name.
+touch /etc/caddy/Caddyfile
+chmod +x /etc/caddy/Caddyfile
+cat >/etc/caddy/Caddyfile <<-EOF	 	
+# The Caddyfile is an easy way to configure your Caddy web server.
+#
+# Unless the file starts with a global options block, the first
+# uncommented line is always the address of your site.
+#
+# To use your own domain name (with automatic HTTPS), first make
+# sure your domain's A/AAAA DNS records are properly pointed to
+# this machine's public IP, then replace ":80" below with your
+# domain name.
 
- #:80 {
+#:80 {
 	# Set this path to your site's directory.
 	#root * /usr/share/caddy
 
