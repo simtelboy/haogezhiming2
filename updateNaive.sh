a56c2da9b92f0b7bdeb298ad3edfe6e0058e6783
simtelboy <95013297+simtelboy@users.noreply.github.com>
2024-03-05 09:39:59 +0800

    Update updateNaive.sh

--- updateNaive.sh
+++ updateNaive.sh
@@ -4,10 +4,10 @@
 
 # 定义变量
 RELEASE_URL="https://github.com/klzgrad/forwardproxy/releases/latest/download/caddy-forwardproxy-naive.tar.xz"
-BIN_PATH="/bin/caddy"
-TMP_DIR="/tmp/caddy-forwardproxy-naive"
+BIN_PATH="/bin"
+TMP_DIR="/tmp"
 TMP_FILE="/tmp/caddy-forwardproxy-naive.tar.xz"
-LOCAL_VERSION=$($BIN_PATH --version 2>&1 | awk '{print $1}')
+LOCAL_VERSION=$($BIN_PATH/caddy --version 2>&1 | awk '{print $1}')
 REMOTE_VERSION=$(curl -s https://api.github.com/repos/klzgrad/forwardproxy/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
 
 # 从版本字符串中仅提取主版本号 vX.Y.Z
@@ -29,10 +29,10 @@ if [ "$LOCAL_VERSION" != "$REMOTE_VERSION" ]; then
     tar -xJf $TMP_FILE -C $TMP_DIR
     
     # 替换旧版本的caddy
-    mv $TMP_DIR/caddy $BIN_PATH
+    mv $TMP_DIR/caddy-forwardproxy-naive/caddy $BIN_PATH
     
     # 确保新版本的caddy具有执行权限
-    chmod +x $BIN_PATH
+    chmod +x $BIN_PATH/caddy
     
     
     
@@ -41,8 +41,8 @@ if [ "$LOCAL_VERSION" != "$REMOTE_VERSION" ]; then
 
     # 清理下载的临时文件和目录
    # rm -f $TMP_FILE
-   # rm -rf $TMP_DIR
-    
+   # rm -rf $TMP_DIR/caddy-forwardproxy-naive
+   
     echo "naiveproxy 更新完成."
 else
     echo "当前版本是最新版本: $LOCAL_VERSION."
